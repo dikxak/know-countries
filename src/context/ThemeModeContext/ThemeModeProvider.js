05144240680a27d920525a2650dc8d26afafa898
dikxak <dikshakpoudel76@gmail.com>
2022-09-27 12:39:46 +0545

    set app theme according to user's theme

--- src/context/ThemeModeContext/ThemeModeProvider.js
+++ src/context/ThemeModeContext/ThemeModeProvider.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import ThemeModeContext from './theme-mode-context';
 
 const ThemeModeProvider = props => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState();
 
   const themeChangeHandler = theme => {
     setTheme(theme);
   };
+
+  useEffect(() => {
+    // If user device's theme is dark, set application color theme to dark
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark');
+    }
+  }, []);
 
   const themeContext = {
     themeMode: theme,
