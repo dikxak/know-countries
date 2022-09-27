c6c6847f8923b7d7c729a6d3026e1a0ab936238d
dikxak <dikshakpoudel76@gmail.com>
2022-09-27 13:03:55 +0545

    format sass files and give final touch

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ import Navbar from '../components/ui/Navbar/Navbar';
 const HomePage = () => {
   return (
     <React.Fragment>
-      <Navbar className="light" />
+      <Navbar />
       <CountryContainer />
     </React.Fragment>
   );
